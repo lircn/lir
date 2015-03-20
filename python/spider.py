fc37a54d4c55a2d75113541964b0144968547994
Li Zheng <lizheng_w5625@tp-link.net>
2015-03-20 16:03:19 +0800

    good

--- python/spider.py
+++ python/spider.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 import urllib2
@@ -7,8 +8,21 @@ import thread
 import time
 import socks
 import socket
+import os
+import platform
 
 from BeautifulSoup import BeautifulSoup
+
+class Utils:
+	def __init__(self):
+		self.system = platform.system()
+
+	def Clear(self):
+		if self.system == 'Windows':
+			os.system('cls')
+		elif self.system == 'Linux':
+			os.system('clear')
+
 
 class SpiderCnbeta:
 	def __init__(self):
@@ -41,8 +55,8 @@ class SpiderCnbeta:
 			else:
 				time.sleep(1)
 
-	def ShowPage(self, nowPage, page):
-		for items in nowPage:
+	def ShowPage(self, fence, page):
+		for items in fence:
 			break
 
 	def Start(self):
@@ -56,7 +70,7 @@ class SpiderCnbeta:
 
 #		while self.enable:
 #			if self.pages:
-#				nowPage = self.pages[0]
+#				fence = self.pages[0]
 #				del self.pages[0]
 
 class SpiderQb:
@@ -64,18 +78,29 @@ class SpiderQb:
 		self.page = 1
 		self.cache = []
 		self.pages = []
-		self.enable = False
+		self.fence = 0
+		self.anchor = 0
+		self.utils = None
+		self.textUrl = 'http://m.qiushibaike.com/text/page/'
+
+	def SetUtils(self, utils):
+		self.utils = utils
 
 	def EnableProxy(self):
 		socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS4, "127.0.0.1", 28888)
 		socket.socket = socks.socksocket
 
 	def GetPage(self, page):
-		url = "http://m.qiushibaike.com/text/page/" + page
+		url = self.textUrl + page
 		userAgent = 'Mozilla/5.0 (Windows NT 6.1;)'
 		headers = {'User-Agent':userAgent}
 		req = urllib2.Request(url, headers = headers)
-		resp = urllib2.urlopen(req)
+		try:
+			resp = urllib2.urlopen(req)
+		except urllib2.HTTPError, e:
+			print u'''
+			%d: %s
+			''' % (e.code, e.reason)
 		page = resp.read()
 
 		soup = BeautifulSoup(page)
@@ -89,37 +114,60 @@ class SpiderQb:
 		return items
 
 	def LoadPage(self):
-		try:
-			self.cache = self.GetPage(str(self.page))
+		self.cache = self.GetPage(str(self.page))
+		if len(self.cache) == 0:
+			print u'''
+			网太高，我上不去...
+			请确保你能访问：%s
+			''' % self.textUrl
+		else:
+			self.pages += self.cache
+			self.anchor = len(self.cache) / 2
 			self.page += 1
-		except:
-			print 'can not connect'
 
 	def ShowPage(self):
-		for item in self.items:
-			print item['time']
-			print item['text'] + '\n'
+		while True:
+			self.utils.Clear()
+			print u'''
+			===============
+			| h: 向上翻页 |
+			| j: 向下翻页 |
+			| q: 退出     |
+			===============
+			'''
+			if self.fence < 0:
+				self.fence = 0
+			if self.fence >= len(self.pages) - self.anchor:
+				print u'''
+			正在自动为您加载更多...
+				'''
+				thread.start_new_thread(self.LoadPage, ())
+
+			i = self.fence
+			for i in range(self.fence, min(self.fence + 5, len(self.pages))):
+				print str(i) + '. ' + self.pages[i]['time']
+				print self.pages[i]['text'] + '\n'
+			print '(' + str(i) + '/' + str(len(self.pages)) + ')'
+			cmd = raw_input('h/j/q:')
+			if cmd == 'h':
+				self.fence -= 5
+			elif cmd == 'j':
+				self.fence += 5
+				if self.fence >= len(self.pages) - 5:
+					self.fence = len(self.pages) - 5
+			elif cmd == 'q':
+				print 'Bye-bye~'
+				break
 
 	def Start(self):
-		self.enable = True
-		page = self.page
-
-		print u'正在加载中...'
-
 		self.LoadPage()
-		thread.start_new_thread(self.LoadPage, ())
-
 		self.ShowPage()
 
-#		while self.enable:
-#			if self.pages:
-#				nowPage = self.pages[0]
-#				del self.pages[0]
 
 print 'Welcome!'
 strIn = raw_input(
-'''Choose a site:
-1. QiuBai
+u'''选个站点吧～:
+1. 糗事百科
 2. CnBeta
 ''')
 
@@ -139,4 +187,5 @@ else:
 	print 'Input error'
 
 if doFlag:
+	spider.SetUtils(Utils())
 	spider.Start()
